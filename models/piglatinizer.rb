6c1409fe845853e61d7e90bea71c716ca85c30da
Kayvan Vakili <kvakili813@gmail.com>
2018-07-30 19:55:58 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -4,14 +4,16 @@ class PigLatinizer
       #return word if %w[and an in].include?(word)
       letters = word.split("")
       letters.keep_if {|letter| letter != "."}
-      if letters.size > 1
+
+      if letters.size == 1 || vowel?(letters[0])
+        letters << "way"
+      elsif letters.size > 1
 
         until vowel?(letters[0])
           letters << letters.shift
         end
         letters  << "ay"
-      elsif letters.size == 1 || vowel?(letters[0])
-        letters << "way"
+    
       end
       letters.join
     end
